0fa830296fc1f918df1f4d5bae4381d30f169d77
mariavaliensi-hub <maria.valiensi@escola.pr.gov.br>
2025-10-30 12:20:48 -0300

    h

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const avanca = document.querySelectorAll(".btn- p")
+const avanca = document.querySelectorAll(".btn- proximo")
 ong
 
+avanca.forEach(button=>{
+   button.adoEventListerner('click', function{
+    const atual= document.querySelector('.ativo')
+  const proximoPasso = 'passo-' + this.getAttribute()}
 
+   )
 
+})
 
 
 
